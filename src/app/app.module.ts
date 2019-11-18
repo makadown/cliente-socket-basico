c7ab6f55e13b0a94735ac0fc981c5b77e36bc70f
Makadown <maseflores79@gmail.com>
2019-11-18 12:49:49 -0600

    Agregando nuevos componentes para seccion 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { environment } from '../environments/environment';
 import { FooterComponent } from './components/footer/footer.component';
 import { ChatComponent } from './components/chat/chat.component';
+import { ListaUsuariosComponent } from './components/lista-usuarios/lista-usuarios.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MensajesComponent } from './pages/mensajes/mensajes.component';
 
 const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
 
@@ -15,7 +18,10 @@ const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
   declarations: [
     AppComponent,
     FooterComponent,
-    ChatComponent
+    ChatComponent,
+    ListaUsuariosComponent,
+    LoginComponent,
+    MensajesComponent
   ],
   imports: [
     BrowserModule,
